972645156d91ed369a0c71bf789ea2ceaeead8c2
Edwin Gao <egao@outlook.com>
2018-11-22 14:23:54 -0800

    Formatting fix for Exceptions classes in __init__.py

--- mozbitbar/__init__.py
+++ mozbitbar/__init__.py
@@ -5,21 +5,25 @@
 from __future__ import print_function, absolute_import
 
 class NotInitializedException(Exception):
-      def __init__(self, message):
-          super(NotInitializedException, self).__init__(message)
+    def __init__(self, message):
+        super(NotInitializedException, self).__init__(message)
 
 class InvalidRecipeException(Exception):
     def __init__(self, message):
-            super(InvalidRecipeException, self).__init__(message)
+        super(InvalidRecipeException, self).__init__(message)
+
+class MissingArgumentException(Exception):
+    def __init__(self, message):
+        super(MissingArgumentException, self).__init__(message)
 
 class FrameworkException(Exception):
     def __init__(self, message):
-            super(FrameworkException, self).__init__(message)
+        super(FrameworkException, self).__init__(message)
 
 class FileException(Exception):
     def __init__(self, message):
-            super(FileException, self).__init__(message)
+        super(FileException, self).__init__(message)
 
 class DataFileException(FileException):
     def __init__(self, message):
-            super(DataFileException, self).__init__(message)
+        super(DataFileException, self).__init__(message)
