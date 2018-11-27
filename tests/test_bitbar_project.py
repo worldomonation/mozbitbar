8bb1b24fdd0e267c11e3375b79135747f70f3f1f
Edwin Gao <egao@outlook.com>
2018-11-27 12:16:36 -0800

    Expanded test cases.

--- tests/test_bitbar_project.py
+++ tests/test_bitbar_project.py
@@ -5,13 +5,33 @@ import pytest
 from mozbitbar.bitbar_project import BitbarProject
 from mozbitbar import ProjectException
 
-@pytest.mark.parametrize('kwargs,expected', [ ({'project_id': 1}, 1) ])
-def test_bitbar_project(mock_bitbar_project, kwargs, expected):
-    project = mock_bitbar_project('existing', **kwargs)
+@pytest.mark.parametrize('kwargs,expected', [
+        ({'project_id': 1}, 1),
+        ({'project_id': 999}, 999),
+        ({'project_id': 2**32}, 2**32),
+        ({'project_id': 0}, 0),
+        ({'project_id': -1}, -1),
+        ])
+def test_bitbar_project(kwargs, expected):
+    """Ensures BitbarProject sets project_id as expected.
+
+    Directly tests methods involved in:
+        - initialization
+        - branching logic based on project_id or project_name
+        - setting of project by project_id
+
+    Indirectly tests methods involved in:
+        - property setter for project_id
+
+    Last call Testdroid.get_project is mocked.
+    """
+    project = BitbarProject('existing', **kwargs)
     assert project.project_id == expected
 
 
 @pytest.mark.parametrize('project_status', ['present', 'exist', 'create'])
 def test_bitbar_project_invalid_status(project_status):
+    """Ensures BitbarProject only accepts 
+    """
     with pytest.raises(ProjectException):
         BitbarProject(project_status)
