cfbc81773a0c09cac478bc877da6d983a1217717
Edwin Gao <egao@outlook.com>
2018-11-27 11:39:19 -0800

    Changes: - added code to mock BitbarProject as well - changed tests to be parametrized

--- tests/test_bitbar_project.py
+++ tests/test_bitbar_project.py
@@ -3,18 +3,15 @@ from __future__ import print_function, absolute_import
 import pytest
 
 from mozbitbar.bitbar_project import BitbarProject
+from mozbitbar import ProjectException
+
+@pytest.mark.parametrize('kwargs,expected', [ ({'project_id': 1}, 1) ])
+def test_bitbar_project(mock_bitbar_project, kwargs, expected):
+    project = mock_bitbar_project('existing', **kwargs)
+    assert project.project_id == expected
 
 
-def test_bitbar_project_invalid_status():
-    with pytest.raises(NotImplementedError):
-        BitbarProject(project_status='unknown')
-
-
-def test_bitbar_project():
-    project = BitbarProject('existing', **{'project_id': 000000})
-    assert project.project_id == 000000
-
-
-# @pytest.mark.parametrize(
-#     'project_status', 'expected', [('existing', )])
-# def test_bitbar_project_status():
+@pytest.mark.parametrize('project_status', ['present', 'exist', 'create'])
+def test_bitbar_project_invalid_status(project_status):
+    with pytest.raises(ProjectException):
+        BitbarProject(project_status)
