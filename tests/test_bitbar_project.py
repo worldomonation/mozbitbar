fa431fb9f4f31440559809296240947f06fda589
Edwin Gao <egao@outlook.com>
2018-12-07 10:51:57 -0500

    - updated test mocked data in conftest - added new mocker for get_projects() methjod in conftest - revamped tests in test_bitbar_project; currently failing 1 test

--- tests/test_bitbar_project.py
+++ tests/test_bitbar_project.py
@@ -1,5 +1,7 @@
 from __future__ import print_function, absolute_import
 
+import string
+
 import pytest
 
 from mozbitbar.bitbar_project import BitbarProject
@@ -7,27 +9,83 @@ from mozbitbar import ProjectException
 
 
 @pytest.mark.parametrize('kwargs,expected', [
-        ({'project_id': 1}, 1),
-        ({'project_id': 999}, 999),
-        ({'project_id': 2**32}, 2**32),
-        ({'project_id': 0}, 0),
-        ({'project_id': -1}, -1),
-        ])
-def test_bitbar_project(kwargs, expected):
-    """Ensures BitbarProject sets project_id as expected.
+    ({'id': 1}, 1),
+    ({'id': 99}, 99),
+])
+def test_bb_project_existing_id(kwargs, expected):
+    """Ensures BitbarProject sets id as expected.
 
     Directly tests methods involved in:
         - initialization
-        - branching logic based on project_id or project_name
-        - setting of project by project_id
+        - setting of project by id
 
     Indirectly tests methods involved in:
-        - property setter for project_id
+        - property setter for id
 
-    Last call Testdroid.get_project is mocked.
+    Last call Testdroid.get_project is mocked to return a pseudo
+    project.
     """
     project = BitbarProject('existing', **kwargs)
     assert project.project_id == expected
+
+
+@pytest.mark.parametrize('kwargs', [
+    {'id': 100},
+    {'id': 2**32},
+    {'id': -1},
+])
+def test_bb_project_existing_invalid_id(kwargs):
+    """Ensures BitbarProject raises an exception on invalid
+    project ID.
+    """
+    with pytest.raises(ProjectException):
+        BitbarProject('existing', **kwargs)
+
+
+@pytest.mark.parametrize('kwargs,expected', [
+        ({'name': 'mock_project'}, 'mock_project')
+        ])
+def test_bb_project_existing_name(kwargs, expected):
+    """Ensures BitbarProject sets name as expected.
+
+    Directly tests methods involved in:
+        - initialization
+        - branching logic based on name or name
+        - setting of project by name
+
+    Indirectly tests methods involved in:
+        - property setter for name
+
+    Last call Testdroid.get_project is mocked to return a pseudo
+    project.
+    """
+    project = BitbarProject('existing', **kwargs)
+    assert project.project_name == expected
+
+
+@pytest.mark.parametrize('kwargs', [
+    {'name': string.lowercase},
+    {'name': 'NULL'},
+    {'name': 'None'},
+])
+def test_bb_project_existing_invalid_name(kwargs):
+    with pytest.raises(ProjectException):
+        BitbarProject('existing', **kwargs)
+
+
+@pytest.mark.parametrize('kwargs,expected', [
+    ({'id': 1, 'name': 'mock_project'}, [1, 'mock_project']),
+    ({'id': 10000, 'name': 'mock_project'},
+     [10000, 'yet_another_mock_project']),
+])
+def test_bb_project_existing_id_and_name(kwargs, expected):
+    """Ensures BitbarProject can accept scenario with both
+    id and name provided. Verifies that existing project selection
+    prioritizes project id over name.
+    """
+    project = BitbarProject('existing', **kwargs)
+    assert project.project_id == expected[0]
+    assert project.project_name == expected[1]
 
 
 @pytest.mark.parametrize('project_status', ['present', 'exist', 'create'])
