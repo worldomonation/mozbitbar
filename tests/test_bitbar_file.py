04018bf92b48c98510ba4af3578e59f5af5b72c6
Edwin Gao <egao@outlook.com>
2018-12-08 21:36:22 -0500

    - stub file to test bitbar file operations

---
+++ tests/test_bitbar_file.py
@@ -0,0 +1,12 @@
+from __future__ import print_function, absolute_import
+
+import string
+
+import pytest
+
+from mozbitbar.bitbar_project import BitbarProject
+from mozbitbar import ProjectException
+
+
+def test_bb_file_on_bitbar():
+    pass
