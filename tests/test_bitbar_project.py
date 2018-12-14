ca99b4b666d9a5784106aa4560eb62f43e618a4b
Edwin Gao <egao@outlook.com>
2018-12-14 14:53:45 -0800

    - refactored out the project template to its own method outside the mocked class - refactored tests in compliance with the parameter names

--- tests/test_bitbar_project.py
+++ tests/test_bitbar_project.py
@@ -11,11 +11,11 @@ from mozbitbar import ProjectException, FrameworkException
 # Project with Existing ID #
 
 @pytest.mark.parametrize('kwargs,expected', [
-    ({'id': 1}, 1),
-    ({'id': 99}, 99),
-    ({'id': 100}, ProjectException),
-    ({'id': 2**32}, ProjectException),
-    ({'id': -1}, ProjectException),
+    ({'project_id': 1}, 1),
+    ({'project_id': 99}, 99),
+    ({'project_id': 100}, ProjectException),
+    ({'project_id': 2**32}, ProjectException),
+    ({'project_id': -1}, ProjectException),
 ])
 def test_bb_project_existing_by_id(kwargs, expected):
     """Ensures BitbarProject sets id as expected.
@@ -40,11 +40,11 @@ def test_bb_project_existing_by_id(kwargs, expected):
 
 
 @pytest.mark.parametrize('kwargs,expected', [
-    ({'name': 'mock_project'}, 'mock_project'),
-    ({'name': 'another_mock_project'}, 'another_mock_project'),
-    ({'name': string.lowercase}, ProjectException),
-    ({'name': 'NULL'}, ProjectException),
-    ({'name': 'None'}, ProjectException),
+    ({'project_name': 'mock_project'}, 'mock_project'),
+    ({'project_name': 'another_mock_project'}, 'another_mock_project'),
+    ({'project_name': string.lowercase}, ProjectException),
+    ({'project_name': 'NULL'}, ProjectException),
+    ({'project_name': 'None'}, ProjectException),
 ])
 def test_bb_project_existing_by_name(kwargs, expected):
     """Ensures BitbarProject sets name as expected.
@@ -70,9 +70,14 @@ def test_bb_project_existing_by_name(kwargs, expected):
 
 
 @pytest.mark.parametrize('kwargs,expected', [
-    ({'id': 1, 'name': 'mock_project'}, [1, 'mock_project']),
-    ({'id': 10000, 'name': 'mock_project'},
-     [10000, 'yet_another_mock_project']),
+    (
+        {'project_id': 1, 'project_name': 'mock_project'},
+        {'project_id': 1, 'project_name': 'mock_project'}
+    ),
+    (
+        {'project_id': 10000, 'project_name': 'mock_project'},
+        {'project_id': 10000, 'project_name': 'yet_another_mock_project'}
+    ),
 ])
 def test_bb_project_existing_id_and_name(kwargs, expected):
     """Ensures BitbarProject can accept scenario with both
@@ -80,8 +85,8 @@ def test_bb_project_existing_id_and_name(kwargs, expected):
     prioritizes project id over name.
     """
     project = BitbarProject('existing', **kwargs)
-    assert project.project_id == expected[0]
-    assert project.project_name == expected[1]
+    assert project.project_id == expected['project_id']
+    assert project.project_name == expected['project_name']
 
 
 # Project status method #
@@ -102,30 +107,32 @@ def test_bb_project_status(project_status, expected):
 # Project with New ID #
 
 
-@pytest.mark.parametrize('kwargs', [
-    ({
-        'name': 'parametrized_project_1',
-        'type': 'parametrized_type'
-    }),
-    ({
-        'name': string.punctuation,
-        'type': string.lowercase
-    })
+@pytest.mark.parametrize('kwargs,expected', [
+    (
+        {'project_name': 'parametrized_project',
+            'project_type': 'parametrized_type'},
+        {'project_name': 'parametrized_project',
+            'project_type': 'parametrized_type'},
+    ),
+    # ({
+    #     'project_name': string.punctuation,
+    #     'project_type': string.lowercase
+    # })
 ])
-def test_bb_project_create_unique_name(kwargs):
+def test_bb_project_create_unique_name(kwargs, expected):
     """Ensures BitbarProject.create_project() is able to create a valid
     project instance given appropriate project name and type.
     """
     project = BitbarProject('new', **kwargs)
     assert project.project_id is not None
-    assert project.project_name == kwargs['name']
-    assert project.project_type == kwargs['type']
+    assert project.project_name == expected['project_name']
+    assert project.project_type == expected['project_type']
 
 
 @pytest.mark.parametrize('kwargs', [
     ({
-        'name': 'mock_project',
-        'type': 'mock_type'
+        'project_name': 'mock_project',
+        'project_type': 'mock_type'
     })
 ])
 def test_bb_project_create_duplicate_name_without_flag(kwargs):
@@ -145,20 +152,23 @@ def test_bb_project_create_duplicate_name_with_flag(kwargs):
 
 
 @pytest.mark.parametrize('kwargs,expected', [
-    ({'id': 12}, FrameworkException),
-    ({'id': 1}, {'name': 'mock_framework', 'id': 1}),
-    ({'name': 'mock_framework'}, {'name': 'mock_framework', 'id': 1}),
-    ({'name': 'mock_framework', 'id': 1}, {'name': 'mock_framework', 'id': 1}),
-    ({'name': u'mock_framework', 'id': 1}, {'name': 'mock_framework', 'id': 1}),
-    ({'name': 'mock_unicode_framework'},
-     {'name': 'mock_unicode_framework', 'id': 2}),
+    ({'framework_id': 12}, FrameworkException),
+    ({'framework_id': 1}, {'framework_name': 'mock_framework', 'framework_id': 1}),
+    ({'framework_name': 'mock_framework'}, {
+     'framework_name': 'mock_framework', 'framework_id': 1}),
+    ({'framework_name': 'mock_framework', 'framework_id': 1}, {
+     'framework_name': 'mock_framework', 'framework_id': 1}),
+    ({'framework_name': u'mock_framework', 'framework_id': 1}, {
+     'framework_name': 'mock_framework', 'framework_id': 1}),
+    ({'framework_name': 'mock_unicode_framework'},
+     {'framework_name': 'mock_unicode_framework', 'framework_id': 2}),
 ])
 def test_bb_project_framework(kwargs, expected):
-    project = BitbarProject('existing', **{'id': 99})
+    project = BitbarProject('existing', **{'project_id': 99})
     if expected == FrameworkException:
         with pytest.raises(FrameworkException):
             project.set_project_framework(**kwargs)
     else:
         project.set_project_framework(**kwargs)
-        assert project.framework_id == expected['id']
-        assert project.framework_name == expected['name']
+        assert project.framework_id == expected['framework_id']
+        assert project.framework_name == expected['framework_name']
