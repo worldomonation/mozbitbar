926d383df39a836f02c0f4fd73b2154426cba3c4
Edwin Gao <egao@outlook.com>
2019-01-07 15:33:05 -0800

    Stub work for device test

---
+++ tests/test_bitbar_project_device.py
@@ -0,0 +1,19 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from __future__ import print_function, absolute_import
+
+import pytest
+
+
+
+
+@pytest.mark.parametrize('kwargs,expected', [
+    (
+        1,
+        1
+    )
+])
+def test_set_device_group(kwargs, expected):
+    pass
