63c6de16e0d2ac1ab92f175d4b9b98ace6e4a498
Edwin Gao <egao@outlook.com>
2019-01-23 11:38:01 -0800

    Minor work

--- tests/test_integration_credentials.py
+++ tests/test_integration_credentials.py
@@ -10,24 +10,6 @@ import yaml
 from mozbitbar import cli
 from mozbitbar.run import run_recipe
 
-_default_recipe = [{
-    'project': 'existing',
-    'arguments': {
-        'project_id': 10101010,
-        'project_name': 'mock_project'
-    }
-}]
-
-_test_credential_file = 'test_credential_file.yaml'
-
-
-@pytest.fixture
-def default_recipe_path(tmpdir):
-    path = tmpdir.mkdir('default_recipe').join('default_recipe.yaml')
-    path.write(yaml.dump(_default_recipe))
-
-    return path.strpath
-
 
 @pytest.mark.parametrize('credentials', [
     (
@@ -37,18 +19,21 @@ def default_recipe_path(tmpdir):
         },
     )
 ])
-def test_bitbar_init_with_credentials(tmpdir, default_recipe_path,
+def test_bitbar_init_with_credentials(tmpdir, base_recipe, write_tmp_file,
                                       credentials):
     cli_argument = []
 
-    # set up recipe first using the fixture
-    cli_argument.extend(['-r', default_recipe_path])
-
     # set up test credential file
-    path = tmpdir.mkdir('mock').join(_test_credential_file)
-    path.write(yaml.dump(credentials))
-    cli_argument.extend(['-c', path.strpath])
+    credentials_path = tmpdir.mkdir('int_test').join('test_credentials.yaml')
+    credentials_path.write(yaml.dump(credentials))
+
+    # write a default recipe
+    recipe_path = write_tmp_file(base_recipe)
+
+    # formulate the simulated cli commands
+    cli_argument.extend(['-c', credentials_path.strpath])
+    cli_argument.extend(['-r', recipe_path.strpath])
 
     # kick off integration test, assertions are built in to the code
     args = cli.cli(cli_argument)
-    run_recipe(default_recipe_path, args)
+    run_recipe(recipe_path.strpath, args)
